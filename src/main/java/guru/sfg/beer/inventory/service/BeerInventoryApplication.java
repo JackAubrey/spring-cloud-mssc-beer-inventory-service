8d5a32af082ecfe1748f6cfb3ea469ee09ec4ff0
danilo <d.cividin@hotmail.it>
2024-04-10 11:27:25 +0200

    update BOM

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -2,12 +2,18 @@ package guru.sfg.beer.inventory.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication(scanBasePackages = {"guru.sfg.beer.common", "guru.sfg.beer.inventory"})
 public class BeerInventoryApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BeerInventoryApplication.class, args);
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void initHook() {
+        Hooks.enableAutomaticContextPropagation();
+    }
 }
